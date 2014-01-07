57b60e9f675df32f9c42b538ec4a23a08ab786fe
David Sullivan <ds381@rusty.phys.susx.ac.uk>
2014-01-07 12:34:05 +0000

    Pause work on r2cpm

--- ramses2cubep3m_v0.01.f90
+++ ramses2cubep3m_v0.01.f90
@@ -14,16 +14,21 @@
 
 program ramses2cube3m
 
+	!use mpi
+
 	implicit none
 
 	include 'mpif.h'
 
 	logical :: debug = .true.
 
-	!! Start memory allocation for ramses -> gadget
+		!! Start memory allocation for ramses -> gadget
 	! counters
 	integer :: i, j, ilevel, idim, idm, istar, ifile, icpu, ivar, ind,col
 	integer :: nfiles, ncpus, ndim, twotondim, firstfile, lastfile
+
+	! flags
+	logical :: gas, stars, interpolate, refined(8), allrefined
 
 	! cosmological and physical parameters
 	real(kind=8)            :: omega_m, omega_l, H0, h, a, gamma, boxsize, dbldummy
@@ -75,13 +80,107 @@ program ramses2cube3m
 	integer         :: mpi_ierr
 	integer(kind=4) :: mpi_reduce_buffer, files_per_cpu
 
-	!! end memory allocation for ramses -> gadget
-
 	! Initialise MPI
 	call mpi_initialise
 
 	! Run ramses2gadget
 	call ramses2gadget
+
+	contains
+
+		subroutine ramses2gadget
+
+			! ramses2gadget version 1.0 !
+			! ========================= !
+			! (c) 2009 by Timur Doumler !
+
+			!! end memory allocation for ramses -> gadget
+
+			if (debug) write(*,*) 'Entering ramses2gadget subroutine'
+
+			! Display hello message
+			if (icpu == 0) call hello
+
+			! Read arguments and set directory name and flags for gas/interpolation mode
+			call read_args(icpu, dir_name, gas, interpolate)
+
+			! Display status message
+			if (icpu == 0) then
+				if (.not.gas) write(*,*) 'Running in dark matter mode.'
+				if (gas.and.(.not.interpolate)) write (*,*) 'Running with hydro in one-particle-per-cell mode.'
+				if (gas.and.interpolate) write (*,*) 'Running with hydro in interpolation mode.'
+			endif
+
+			! Open the info_....txt file in order to read cosmological parameters
+
+			! Construct filename, verify existence and assign I/O unit number
+			! dir_number=dir_name( index(dir_name,'output_')+7 : index(dir_name,'output_')+13 ) 
+			dir_number='00001'
+			info_filename   = trim(dir_name) // '/info_'   // trim(dir_number) // '.txt'
+			call inquire_file (info_filename)
+			info_file   = 988
+
+			! in case we have gas, we need to open some more files and gather some more parameters...
+			if (gas) then 
+				!Open cpu1 file to verify existence and assign I/O unit number
+				amr_filename    = trim(dir_name) // '/amr_'    // trim(dir_number) // '.out00001'
+				hydro_filename  = trim(dir_name) // '/hydro_'  // trim(dir_number) // '.out00001'
+				call inquire_file (amr_filename)
+				call inquire_file (hydro_filename)  
+				amr_file    = 989
+				hydro_file  = 990  
+			end if
+
+			!Open and read info....txt file
+			open(unit=info_file, file=info_filename, form='formatted', status='old', action='read', position='rewind')
+			read(info_file,'(13X,I11)')    nfiles
+			read(info_file,'(13X,I11)')    ndim
+			read(info_file,*)              ! skip levelmin
+			read(info_file,*)              ! skip levelmax
+			read(info_file,*)              ! skip ngridmax
+			read(info_file,*)              ! skip nstep_coarse
+			read(info_file,*)              ! skip empty line
+			read(info_file,'(13X,E23.15)') boxsize ! called 'boxlen' in the file
+			read(info_file,*)              ! skip time
+			read(info_file,'(13X,E23.15)') a ! called 'aexp' in the file
+			read(info_file,'(13X,E23.15)') H0
+			read(info_file,'(13X,E23.15)') omega_m
+			read(info_file,'(13X,E23.15)') omega_l
+			read(info_file,*)              ! skip omega_k
+			read(info_file,*)              ! skip omega_b
+			read(info_file,'(13X,E23.15)') ramses_length_unit
+			read(info_file,'(13X,E23.15)') ramses_dens_unit
+			read(info_file,'(13X,E23.15)') ramses_time_unit
+			read(info_file,*)              ! skip empty line
+			read(info_file,'(14X,A80)')    ordering
+			close(info_file)
+
+			if (gas) then
+				! open first amr file (.out00001) and read some required quantities from the header
+				open(unit=amr_file, file=amr_filename,status='old', form='unformatted', action='read', position='rewind')
+				read(amr_file)  ! skip nfiles
+				read(amr_file)  ! skip ndim
+				read(amr_file)  nx, ny, nz
+				read(amr_file)  nlevelmax
+				read(amr_file)  ! skip ngridmax
+				read(amr_file)  nboundary
+				close(amr_file)
+
+				! open first hydro file (.out00001) and read gamma, this is needed for unit conversion
+				open(unit=hydro_file, file=hydro_filename, status='old', form='unformatted', action='read', position='rewind')
+				read(hydro_file) ! skip nfiles
+				read(hydro_file) ! skip nvar
+				read(hydro_file) ! skip ndim
+				read(hydro_file) ! skip nlevelmax
+				read(hydro_file) ! skip nboundary
+				read(hydro_file) gamma
+				close(hydro_file)
+			end if
+
+			! display status message
+			if (icpu==0) write(*,'(A,I4,A,I4,A)') ' Working with', ncpus, ' cpu(s) on', nfiles, ' fileset(s).'
+
+		end subroutine ramses2gadget
 
 end program ramses2cube3m
 
@@ -89,107 +188,28 @@ end program ramses2cube3m
 
 !------------------------------------------------------------------------------------------------------------!
 
-subroutine ramses2gadget
-
-	! ramses2gadget version 1.0 !
-	! ========================= !
-	! (c) 2009 by Timur Doumler !
-
-	if (debug) write(*,*) 'Entering ramses2gadget subroutine'
-
-	! Display hello message
-	if (icpu == 0) call hello
-
-	! Read arguments and set directory name and flags for gas/interpolation mode
-	call read_args(icpu, dir_name, gas, interpolate)
-
-	! Display status message
-	if (icpu == 0) then
-		if (.not.gas) write(*,*) 'Running in dark matter mode.'
-		if (gas.and.(.not.interpolate)) write (*,*) 'Running with hydro in one-particle-per-cell mode.'
-		if (gas.and.interpolate) write (*,*) 'Running with hydro in interpolation mode.'
-	endif
-
-	! Open the info_....txt file in order to read cosmological parameters
-
-	! Construct filename, verify existence and assign I/O unit number
-	! dir_number=dir_name( index(dir_name,'output_')+7 : index(dir_name,'output_')+13 ) 
-	dir_number='00001'
-	info_filename   = trim(dir_name) // '/info_'   // trim(dir_number) // '.txt'
-	call inquire_file (info_filename)
-	info_file   = 988
-
-	! in case we have gas, we need to open some more files and gather some more parameters...
-	if (gas) then 
-		!Open cpu1 file to verify existence and assign I/O unit number
-		amr_filename    = trim(dir_name) // '/amr_'    // trim(dir_number) // '.out00001'
-		hydro_filename  = trim(dir_name) // '/hydro_'  // trim(dir_number) // '.out00001'
-		call inquire_file (amr_filename)
-		call inquire_file (hydro_filename)  
-		amr_file    = 989
-		hydro_file  = 990  
-	end if
-
-	!Open and read info....txt file
-	open(unit=info_file, file=info_filename, form='formatted', status='old', action='read', position='rewind')
-	read(info_file,'(13X,I11)')    nfiles
-	read(info_file,'(13X,I11)')    ndim
-	read(info_file,*)              ! skip levelmin
-	read(info_file,*)              ! skip levelmax
-	read(info_file,*)              ! skip ngridmax
-	read(info_file,*)              ! skip nstep_coarse
-	read(info_file,*)              ! skip empty line
-	read(info_file,'(13X,E23.15)') boxsize ! called 'boxlen' in the file
-	read(info_file,*)              ! skip time
-	read(info_file,'(13X,E23.15)') a ! called 'aexp' in the file
-	read(info_file,'(13X,E23.15)') H0
-	read(info_file,'(13X,E23.15)') omega_m
-	read(info_file,'(13X,E23.15)') omega_l
-	read(info_file,*)              ! skip omega_k
-	read(info_file,*)              ! skip omega_b
-	read(info_file,'(13X,E23.15)') ramses_length_unit
-	read(info_file,'(13X,E23.15)') ramses_dens_unit
-	read(info_file,'(13X,E23.15)') ramses_time_unit
-	read(info_file,*)              ! skip empty line
-	read(info_file,'(14X,A80)')    ordering
-	close(info_file)
-
-	if (gas) then
-		! open first amr file (.out00001) and read some required quantities from the header
-		open(unit=amr_file, file=amr_filename,status='old', form='unformatted', action='read', position='rewind')
-		read(amr_file)  ! skip nfiles
-		read(amr_file)  ! skip ndim
-		read(amr_file)  nx, ny, nz
-		read(amr_file)  nlevelmax
-		read(amr_file)  ! skip ngridmax
-		read(amr_file)  nboundary
-		close(amr_file)
-
-		! open first hydro file (.out00001) and read gamma, this is needed for unit conversion
-		open(unit=hydro_file, file=hydro_filename, status='old', form='unformatted', action='read', position='rewind')
-		read(hydro_file) ! skip nfiles
-		read(hydro_file) ! skip nvar
-		read(hydro_file) ! skip ndim
-		read(hydro_file) ! skip nlevelmax
-		read(hydro_file) ! skip nboundary
-		read(hydro_file) gamma
-		close(hydro_file)
-	end if
-
-	! display status message
-	if (icpu==0) write(*,'(A,I4,A,I4,A)') ' Working with', ncpus, ' cpu(s) on', nfiles, ' fileset(s).'
-
-end subroutine ramses2gadget
-
-!------------------------------------------------------------------------------------------------------------!
-
 subroutine mpi_initialise
 
 	!! ramses2gadget version for the time being
 
-	call MPI_INIT(mpi_ierr)
-	call MPI_COMM_RANK(MPI_COMM_WORLD, icpu, mpi_ierr)
-	call MPI_COMM_SIZE(MPI_COMM_WORLD, ncpus, mpi_ierr)
+	!call MPI_INIT(mpi_ierr)
+	!call MPI_COMM_RANK(MPI_COMM_WORLD, icpu, mpi_ierr)
+	!call MPI_COMM_SIZE(MPI_COMM_WORLD, ncpus, mpi_ierr)
+
+	call mpi_init(ierr)
+    if (ierr /= mpi_success) call mpi_abort(mpi_comm_world,ierr,ierr)
+
+    call mpi_comm_size(mpi_comm_world,nodes_returned,ierr)
+    if (ierr /= mpi_success) call mpi_abort(mpi_comm_world,ierr,ierr)
+
+    if (nodes_returned /= nodes ) then
+      write(*,*) 'IC converter compiled for a different number of nodes'
+      write(*,*) 'mpirun nodes=',nodes_returned,'converter nodes=',nodes 
+      call mpi_abort(mpi_comm_world,ierr,ierr)
+    endif
+
+    call mpi_comm_rank(mpi_comm_world,rank,ierr)
+    if (ierr /= mpi_success) call mpi_abort(mpi_comm_world,ierr,ierr)
 
 end subroutine mpi_initialise
 
